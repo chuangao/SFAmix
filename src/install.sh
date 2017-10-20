13ae2b1cb30666979ed497d00add9cea8a7d15e0
chuangao <chuan.gao.cornell@gmail.com>
2017-10-20 03:47:40 -0400

    changed the pwd variable in intall.sh

--- src/install.sh
+++ src/install.sh
@@ -12,10 +12,10 @@ cd gsl-2.2
 ./configure --prefix=$PWD
 make
 make install
-cp -r include/gsl ${PWD%/*}/include/
-cp -r lib/* ${PWD%/*}/lib/
+cp -r include/gsl ../include/
+cp -r lib/* ../lib/
 
-cd ${PWD%/*}
+cd ../
 
 #URL='http://bitbucket.org/eigen/eigen/get/3.3.3.tar.gz'
 #wget ${URL}
@@ -23,20 +23,20 @@ cd ${PWD%/*}
 #tar -xzvf eigen-eigen-67e894c6cd8f.tar.gz
 #cp -r eigen-eigen-67e894c6cd8f/Eigen $PWD/include/ 
 
-mkdir -p $PWD/include/R
+mkdir -p ./include/R
 Rhome=$(R RHOME)
-cp -r ${Rhome}/include/* $PWD/include/R/
-cp -r ${Rhome}/lib/* $PWD/lib/
+cp -r ${Rhome}/include/* ./include/R/
+cp -r ${Rhome}/lib/* .//lib/
 
 ls
 
-if [[ $OSTYPE == "darwin"* ]]; then
+#if [[ $OSTYPE == "darwin"* ]]; then
 
-    install_name_tool -change ${PWD}/SFAmix/R/gsl-2.2/lib/libgsl.19.dylib ${PWD}/SFAmix/src/lib/libgsl.19.dylib SFAmix.so
-    install_name_tool -change ${PWD}/SFAmix/R/gsl-2.2/lib/libgslcblas.0.dylib ${PWD}/SFAmix/src/lib/libgslcblas.0.dylib SFAmix.so
+#    install_name_tool -change ~/SFAmix/R/gsl-2.2/lib/libgsl.19.dylib ${PWD}/SFAmix/src/lib/libgsl.19.dylib SFAmix.so
+#    install_name_tool -change ~/SFAmix/R/gsl-2.2/lib/libgslcblas.0.dylib ${PWD}/SFAmix/src/lib/libgslcblas.0.dylib SFAmix.so
 
-fi
+#fi
 
-ls
+#ls
 
 
